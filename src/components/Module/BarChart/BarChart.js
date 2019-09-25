c5acd1447cadd70b6e0c94f971f6348975d45f15
Suen Kai Ching <aaron.suen@invbots.com>
2019-09-25 02:12:23 -0400

    UPDATE: responsive attemp

--- src/components/Module/BarChart/BarChart.js
+++ src/components/Module/BarChart/BarChart.js
@@ -1,26 +1,41 @@
 import React, { useEffect, useState, useContext } from 'react'
 import { WindowDimensionsContext } from '../../../context/window-dimensions-context'
 import * as d3 from 'd3'
+import useResponsivefy from '../../../hooks/useResponsivefy'
 
 const BarChart = props => {
-
   const [svg, setSVG] = useState(null)
   const { chartId } = props
   const context = useContext(WindowDimensionsContext)
-  console.log({ context })
+  const [width, setWidth] = useState(null)
+  const [height, setHeight] = useState(null)
+
+  const target = useResponsivefy(svg)
+  if (!height && !width) {
+    if (target) {
+      const { targetWidth, targetHeight } = target
+      setWidth(targetWidth)
+      setHeight(targetHeight)
+    }
+  }
 
   useEffect(() => {
-    const margin = {top: 40, right: 30, bottom: 30, left: 50},
-      width = 460 - margin.left - margin.right,
-      height = 320 - margin.top - margin.bottom;
+    const margin = { top: 40, right: 30, bottom: 30, left: 50 }
+    // const width = 460 - margin.left - margin.right,
+    // height = 320 - margin.top - margin.bottom
     const { data } = props
     console.log({ data })
-    if (!svg || !data) {
-      const svgSelector = d3.select(`#${chartId}`)
-        .append('svg')
-      setSVG(svgSelector)
-    } else drawChart(svg, { ...props, width, height, margin })
-  }, [svg, chartId, props, props.data])
+    if (context.width && context.height) {
+      console.log('hohoho')
+      if ((!svg || !data)) {
+        console.log('123123123')
+        const chart = d3.select(`#${chartId}`)
+        const svgSelector = chart
+          .append('svg')
+        setSVG(svgSelector)
+      } else { drawChart(svg, { ...props, margin, height, width }) }
+    }
+  }, [svg, chartId, props, props.data, context.width, context.height, height, width])
 
   return (
     <div style={{ padding: '2rem' }} id={chartId} />
@@ -28,11 +43,12 @@ const BarChart = props => {
 }
 
 const drawChart = (svg, { data, xAxisLabels, yAxisLables, width, height, margin }) => {
-  var greyColor = "#898989"
-  var barColor = d3.interpolateInferno(0.4);
-  var highlightColor = d3.interpolateInferno(0.3);
-  svg.attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
+  console.log('Here')
+  console.log({ height, width })
+  var greyColor = '#898989'
+  var barColor = d3.interpolateInferno(0.4)
+  var highlightColor = d3.interpolateInferno(0.3)
+  svg
     .append('g')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
   const x = d3.scaleBand()
@@ -56,42 +72,42 @@ const drawChart = (svg, { data, xAxisLabels, yAxisLables, width, height, margin 
         .data(data)
         .enter().append('rect')
         .attr('class', 'bar')
-        .style('display', d => { return d.value === null ? 'none' : null; })
+        .style('display', d => { return d.value === null ? 'none' : null })
         .style('fill',  d => {
-            return d.value === d3.max(data,  d => { return d.value; })
+            return d.value === d3.max(data,  d => { return d.value })
             ? highlightColor : barColor
             })
-        .attr('x',  d => { return x(d.label); })
+        .attr('x',  d => { return x(d.label) })
         .attr('width', x.bandwidth())
-            .attr('y',  d => { return height; })
+            .attr('y',  d => { return height })
             .attr('height', 0)
                 .transition()
                 .duration(750)
                 .delay(function (d, i) {
-                    return i * 150;
+                    return i * 150
                 })
-        .attr('y',  d => { return y(d.value); })
-        .attr('height',  d => { return height - y(d.value); });
+        .attr('y',  d => { return y(d.value) })
+        .attr('height',  d => { return height - y(d.value) })
 
         svg.selectAll('.label')
             .data(data)
             .enter()
             .append('text')
             .attr('class', 'label')
-            .style('display',  d => { return d.value === null ? 'none' : null; })
-            .attr('x', ( d => { return x(d.label) + (x.bandwidth() / 2) -8 ; }))
+            .style('display',  d => { return d.value === null ? 'none' : null })
+            .attr('x', ( d => { return x(d.label) + (x.bandwidth() / 2) - 8  }))
                 .style('fill',  d => {
-                    return d.value === d3.max(data,  d => { return d.value; })
+                    return d.value === d3.max(data,  d => { return d.value })
                     ? highlightColor : greyColor
                     })
-            .attr('y',  d => { return height; })
+            .attr('y',  d => { return height })
                 .attr('height', 0)
                     .transition()
                     .duration(750)
-                    .delay((d, i) => { return i * 150; })
+                    .delay((d, i) => { return i * 150 })
             .text( d => { return d.value })
-            .attr('y',  d => { return y(d.value) + .1; })
-            .attr('dy', '-.7em');
+            .attr('y',  d => { return y(d.value) + .1 })
+            .attr('dy', '-.7em')
 }
 
 export default BarChart
---
+++ src/hooks/useResponsivefy.js
@@ -0,0 +1,40 @@
+import * as d3 from 'd3'
+
+const responsivefy = (svg) => {
+  // get container + svg aspect ratio
+  if (!svg) return null
+  console.log('1231rtergfhgndsgbx')
+  const container = d3.select(svg.node().parentNode)
+  const width = parseInt(svg.style('width'))
+  const height = parseInt(svg.style('height'))
+  const aspect = width / height
+  const { width: targetWidth, height: targetHeight } = getTargetDimension()
+
+  // add viewBox and preserveAspectRatio properties,
+  // and call resize so that svg resizes on inital page load
+  svg.attr('viewBox', '0 0 ' + width + ' ' + height)
+    .attr('perserveAspectRatio', 'xMinYMid')
+    .call(() => resize(targetWidth, targetHeight))
+
+  // to register multiple listeners for same event type,
+  // you need to add namespace, i.e., 'click.foo'
+  // necessary if you call invoke this function for multiple svgs
+  // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+  d3.select(window).on('resize.' + container.attr('id'), resize)
+
+  // get width of container and resize svg to fit it
+  function getTargetDimension () {
+    const targetWidth = parseInt(container.style('width'))
+    const targetHeight = Math.round(targetWidth / aspect)
+    return { width: targetWidth, height: targetHeight }
+  }
+
+  function resize (targetWidth, targetHeight) {
+    svg.attr('width', targetWidth)
+    svg.attr('height', targetHeight)
+  }
+
+  return { targetWidth, targetHeight }
+}
+
+export default responsivefy
